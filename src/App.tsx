6aef98cd9c38ce40b543828eb5d5d8dce0110912
BearXmX <1448862313@qq.com>
2022-08-27 23:19:24 +0800

    init common project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{ margin: '0 auto', color: 'red', fontSize: 18, marginTop: 30 }}>调试 React 项目 专用</h1>
+    </div>
+  )
+}
+
+export default App

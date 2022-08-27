fdea043a5274a3f00cf0aeff5aeeb83171559be4
BearXmX <1448862313@qq.com>
2022-08-27 15:33:56 +0000

    update src/App.tsx.
    Signed-off-by: BearXmX <1448862313@qq.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <h1 style={{ margin: '0 auto', color: 'red', fontSize: 18, marginTop: 30 }}>调试 React 项目 专用</h1>
+      <h1 style={{ margin: '0 auto', color: 'red', fontSize: 18, marginTop: 30, textAlign: 'center' }}>调试 React 项目 专用</h1>
     </div>
   )
 }
